bcdbff0a3472007a93134ce774d93c9aa538a515
winster2004 <winstermano74@gmail.com>
2025-09-21 19:32:49 +0530

    Time: 0 ms (100%), Space: 45.6 MB (21.65%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int n=nums.length;
+        int totalSum=n * (n+1)/2;
+        int arraySum=0;
+
+        for(int num:nums){
+            arraySum +=num;
+        }
+
+        return totalSum-arraySum;
+        
+    }
+}
